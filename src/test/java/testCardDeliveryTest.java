ec5583b6414bb359f38d79efb45bff30ed4b5ea2
ArtemLC <aclutskij@me.com>
2019-11-05 22:58:02 +0300

    add annotation @DisplayName

--- src/test/java/testCardDeliveryTest.java
+++ src/test/java/testCardDeliveryTest.java
@@ -1,17 +1,17 @@
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
-import static org.junit.jupiter.api.Assertions.*;
 
 class testCardDeliveryTest {
 
     @Test
-    void checkOrderFormDeliveryCard() {
+    @DisplayName(value = "Check verification form with correct input")
+    void checkVerificationFormWithCorrectInput() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Грозный");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
